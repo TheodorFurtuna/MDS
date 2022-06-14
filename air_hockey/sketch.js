92fc894b9b7dcf092984a2ea9857b10ae11b6b26
almarfeta <80604680+almarfeta@users.noreply.github.com>
2022-06-14 18:24:43 +0300

    Update sketch.js

--- air_hockey/sketch.js
+++ air_hockey/sketch.js
@@ -6,33 +6,6 @@ let button = document.getElementById("start");
 let mesaj = document.getElementById("mesaj");
 button.addEventListener("click", () => pressed = true);
 
-function DatabaseConnection () {
-
-    let databaseInstance = null;
-    
-    // tracks the number of instances created at a certain time
-    let count = 0;
-    
-    function init() {
-    console.log(`Opening database #${count + 1}`);
-    //now perform operation
-    }
-    function createIntance() {
-    if(databaseInstance == null) {
-    databaseInstance = init();
-    }
-    return databaseInstance;
-    }
-    function closeIntance() {
-    console.log('closing database');
-    databaseInstance = null;
-    }
-    return {
-    open: createIntance,
-    close: closeIntance
-    }
-}
-
 function setup() {
     createCanvas(windowWidth / 2, windowHeight / 2);
     frameRate(60);
